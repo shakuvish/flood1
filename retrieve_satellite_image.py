8aee84f90789f1a66c1ccb159e8c90ddf127caf2
shaku <109548339+shakuvish@users.noreply.github.com>
2025-03-20 10:45:07 +0530

    Update retrieve_satellite_image.py

--- retrieve_satellite_image.py
+++ retrieve_satellite_image.py
@@ -11,12 +11,10 @@ import cartopy.feature
 import matplotlib.colors as clr
 
 def plot_river(ax, lat, lon, river, filename, vmin=None, vmax=None, cbar=True):
-    # new_cmap = clr.LinearSegmentedColormap.from_list('blues', arr, N=256)
     plt.pcolormesh(lat, lon, river, vmin=vmin, vmax=vmax, transform=ccrs.PlateCarree())
     if cbar:
         cbar = plt.colorbar(shrink=0.7, orientation="horizontal");
         cbar.set_label('Mean river discharge (m3/s)')
-    # plt.show()
     plt.savefig(filename)
 
 def main(lat, lon, river, filename):
@@ -33,12 +31,6 @@ def main(lat, lon, river, filename):
 
 rootgrp = netCDF4.Dataset("./satellite_data/" + "rain" + ".nc4")
 
-# lower_lat, upper_lat = 500, 850
-# lower_lon, upper_lon = 2400, 2800
-
-# lat = rootgrp.variables['lat'][lower_lat:upper_lat]
-# lon = rootgrp.variables['lon'][lower_lon:upper_lon]
-# river = rootgrp.variables["precipitationCal"][0][lower_lat:upper_lat, lower_lon:upper_lon]
 
 
 lat = rootgrp.variables['lat'][:]
